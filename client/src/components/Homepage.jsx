af3c99d9555259f7dba19c633db24da9afa96d16
Jhanvi <jhanvi.ug20@nsut.ac.in>
2022-01-25 21:01:56 +0530

    removing logout

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
 
 function Homepage () {
-  const navigate = useNavigate()
+ 
   return (
     <div className='homepage container'>
       <h1>Success</h1>
-      <input
-        type='button'
-        value='Log Out'
-        name='log-out'
-        onClick={() => navigate('/', { replace: true })}
-      ></input>
+     
     </div>
   )
 }
